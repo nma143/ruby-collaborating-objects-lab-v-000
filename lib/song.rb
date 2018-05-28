871a62ff5bf0ec1b98a537d1b5a7de1ac655007a
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-28 22:06:26 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,8 +12,10 @@ def self.new_by_filename(some_filename)
   filename_parts = some_filename.split(" - ")
   new_song = Song.new(filename_parts[1])
   new_song.artist = Artist.find_or_create_by_name(filename_parts[0])
-  new_song.artist.add_song(new_song)
-  new_song.artist.save
+  if new_song.artist
+    new_song.artist.add_song(new_song)
+    new_song.artist.save
+  end
 end
 
 def artist_name= (artistname)
