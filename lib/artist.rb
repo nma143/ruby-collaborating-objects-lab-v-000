609ac12a938e079842145663a5a9bd62e70399b2
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-28 21:55:59 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,51 @@
+class Artist
+
+  @@all = []
+  attr_accessor :name, :songs
+
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+
+    match = 0
+    @@all.each do |artist|
+      if artist.name == name
+        match =1
+      end
+    end
+
+    if match==0
+      new_artist = Artist.new(name)
+    end
+
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts "#{song.name}"
+    end
+  end
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+class Song
+
+attr_accessor :name, :artist
+
+def initialize (name)
+  @name = name
+
+end
+
+def self.new_by_filename(some_filename)
+
+  filename_parts = some_filename.split(" - ")
+  new_song = Song.new(filename_parts[1])
+  new_song.artist = Artist.new(filename_parts[0])
+end
+
+def artist_name= (artistname)
+  self.artist.name = artistname
+end
+
+end
