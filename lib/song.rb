0130cc04b26a5e01327f94578533a4b05d3d3855
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-28 23:15:05 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,23 +13,12 @@ def self.new_by_filename(some_filename)
   filename_parts = some_filename.split(" - ")
   puts "#{filename_parts}"
   new_song = Song.new(filename_parts[1])
-  if filename_parts[0] == "Real Estate"
-    puts "jkfjkfdjkdfjkldfkj;kda"
-  end
   puts "#{filename_parts[0]}"
   new_song.artist = Artist.find_or_create_by_name(filename_parts[0])
-  puts "boooo #{new_song.artist}"
-  #binding.pry
-  #if new_song.artist ==1
-    new_song.artist.add_song(new_song)
-    puts "made song #{new_song.name}"
-    #new_song.artist.save
-  #else
-    #new_song.artist = Artist.new(filename_parts[0])
-    #new_song.artist.add_song(new_song)
-    #new_song.artist.save
 
-  #end
+  new_song.artist.add_song(new_song)
+  puts "made song #{new_song.name}"
+
 end
 
 def artist_name= (artistname)
