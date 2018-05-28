0f7b0690b4b6e071a04fc8e357ecf8db9ce13525
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-05-28 23:07:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,7 +18,7 @@ def self.new_by_filename(some_filename)
   end
   puts "#{filename_parts[0]}"
   new_song.artist = Artist.find_or_create_by_name(filename_parts[0])
-  puts "#{new_song.artist}"
+  puts "boooo #{new_song.artist}"
   #binding.pry
   #if new_song.artist ==1
     new_song.artist.add_song(new_song)
